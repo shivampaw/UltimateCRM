a820ad4e32ef937547911598c3c2e20a2d58ee9a
Laravel Shift <shift@laravelshift.com>
2016-11-21 20:01:52 +0000

    Shift cleanup

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-use Notifiable;
+    use Notifiable;
     /**
      * The attributes that are mass assignable.
      *
