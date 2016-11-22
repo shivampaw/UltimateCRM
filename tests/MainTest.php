ea37c553624496b072d1b34e45c8d146dddff795
Shivam Paw <shivam@shivampaw.com>
2016-11-22 15:33:46 +0000

    Possible to set database for sqlite via .env. Made PHPUnit use a testing sqlite databse in memory for tests. Renamed ExampleTest to MainTest. MainTest uses testing database to create a user and delete it.

---
+++ tests/MainTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class MainTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Test the homepage returns ok.
+     *
+     * @return void
+     */
+    public function testHomePageResponse()
+    {
+        $this->visit('/')
+             ->assertResponseOk();
+    }
+
+    /**
+     * Add a user and then try to delete it.
+     *
+     * @return void
+     */
+    public function testAddAndDeleteUser()
+    {
+        fwrite(STDERR, print_r(PHP_EOL.'Starting Test', true));
+        fwrite(STDERR, print_r(PHP_EOL.'Number of users: '.User::count(), true));
+        $user = new User();
+        $password = str_random(10);
+        $user->name = 'PHPUnitTest';
+        $user->password = bcrypt($password);
+        $user->email = 'PHPUnitTest@PHPUnitTest.com';
+        $user->is_admin = true;
+        $user->save();
+        fwrite(STDERR, print_r(PHP_EOL.'Added user with ID: '.$user->id, true));
+        fwrite(STDERR, print_r(PHP_EOL.'Number of users: '.User::count(), true));
+        $user->delete();
+        fwrite(STDERR, print_r(PHP_EOL.'Deleted user with ID: '.$user->id, true));
+        fwrite(STDERR, print_r(PHP_EOL.'Number of users: '.User::count(), true));
+    }
+}
