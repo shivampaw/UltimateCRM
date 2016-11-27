d41d458ef77cf57a896fd8b3dcf667d849ca7db0
Shivam Paw <shivam@shivampaw.com>
2016-11-27 16:08:11 +0000

    Making sure testing user is deleted with phpunit

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -33,10 +33,13 @@ class MainTest extends TestCase
         fwrite(STDERR, print_r(PHP_EOL.'Number of users: '.User::count(), true));
         
         $user = (new StoreUserRequest())->storeUser("phpunit", "phpunit@example.com");
-
         fwrite(STDERR, print_r(PHP_EOL.'Added user with ID: '.$user->id, true));
+       
         fwrite(STDERR, print_r(PHP_EOL.'Number of users: '.User::count(), true));
+
+        $user->delete();
         fwrite(STDERR, print_r(PHP_EOL.'Deleted user with ID: '.$user->id, true));
+
         fwrite(STDERR, print_r(PHP_EOL.'Number of users: '.User::count(), true));
     }
 }
