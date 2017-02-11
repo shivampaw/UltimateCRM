d8c916f0907807f7d4f086f281aa4b5daae7d708
Shivam Paw <shivam@shivampaw.com>
2017-02-11 15:20:52 +0000

    PHP-CS-FIXER and fixed bug which made deleting clients fail.

--- config/database.php
+++ config/database.php
@@ -116,9 +116,9 @@ return [
     'clusters' => [
         'default' => [
             [
-                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'host'     => env('REDIS_HOST', '127.0.0.1'),
                 'password' => env('REDIS_PASSWORD', null),
-                'port' => env('REDIS_PORT', 6379),
+                'port'     => env('REDIS_PORT', 6379),
                 'database' => 0,
             ],
         ],
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-    
+
     <!-- Styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/app.css">
@@ -17,46 +17,42 @@
 <body id="app-layout">
 
     <nav class="navbar navbar-light bg-faded navbar-static-top navbar-toggleable-sm">
-
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#nav-content" aria-expanded="false">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="navbar-brand" href="{{ url('/') }}">{{ config('crm.site_title') }}</a>
-            
-            <div id="nav-content" class="collapse navbar-collapse">
-                <ul class="navbar-nav mr-auto mt-2 mt-md-0">
-                    <li class="nav-item"><a href="{{ url('/') }}" class="nav-link">Home</a></li>
-                    
-                    @if ( Auth::user() && (Auth::user()->isSuperAdmin()) )
-                        <li class="nav-item"><a href="{{ url('/admins') }}" class="nav-link">Admins</a></li>
-                    @endif
-                    @if ( Auth::user() && (Auth::user()->isAdmin()) )
-                        <li class="nav-item"><a href="{{ url('/clients') }}" class="nav-link">Clients</a></li>
-                    @elseif ( Auth::user() && (!Auth::user()->isAdmin()) )
-                        <li class="nav-item"><a href="{{ url('/invoices') }}" class="nav-link">Invoices</a></li>
-                        <li class="nav-item"><a href="{{ url('/projects') }}" class="nav-link">Projects</a></li>
-                    @endif
-
-                </ul>
-                <ul class="navbar-nav my-2 my-md-0">
-                    @if ( Auth::guest() )
-                        <li class="nav-item"><a href="{{ url('/login') }}" class="nav-link">Login</a></li>
-                    @else
-                        <li class="dropdown nav-item">
-                            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button"aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li class="dropdown-item"><a href="{{ url('/update-password') }}"><i class="fa fa-btn fa-cog"></i>Update Password</a></li>
-                                <li class="dropdown-item">
-                                    <a href="{{ url('/logout') }}" data-method="post" data-token="{{ csrf_token() }}"><i class="fa fa-btn fa-sign-out"></i> Logout</a>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#nav-content" aria-expanded="false">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="{{ url('/') }}">{{ config('crm.site_title') }}</a>
+        <div id="nav-content" class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+                <li class="nav-item"><a href="{{ url('/') }}" class="nav-link">Home</a></li>
+                
+                @if ( Auth::user() && (Auth::user()->isSuperAdmin()) )
+                    <li class="nav-item"><a href="{{ url('/admins') }}" class="nav-link">Admins</a></li>
+                @endif
+                @if ( Auth::user() && (Auth::user()->isAdmin()) )
+                    <li class="nav-item"><a href="{{ url('/clients') }}" class="nav-link">Clients</a></li>
+                @elseif ( Auth::user() && (!Auth::user()->isAdmin()) )
+                    <li class="nav-item"><a href="{{ url('/invoices') }}" class="nav-link">Invoices</a></li>
+                    <li class="nav-item"><a href="{{ url('/projects') }}" class="nav-link">Projects</a></li>
+                @endif
+            </ul>
+            <ul class="navbar-nav my-2 my-md-0">
+                @if ( Auth::guest() )
+                    <li class="nav-item"><a href="{{ url('/login') }}" class="nav-link">Login</a></li>
+                @else
+                    <li class="dropdown nav-item">
+                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button"aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li class="dropdown-item"><a href="{{ url('/update-password') }}"><i class="fa fa-btn fa-cog"></i>Update Password</a></li>
+                            <li class="dropdown-item">
+                                <a href="{{ url('/logout') }}" data-method="post" data-token="{{ csrf_token() }}"><i class="fa fa-btn fa-sign-out"></i> Logout</a>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
     </nav>
 
     <div class="container">
