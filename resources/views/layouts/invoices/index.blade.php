14e26874e4fc74bfa1eddfbd45c894c9f536a620
Shivam Paw <shivam@shivampaw.com>
2017-05-28 18:36:13 +0100

    UI improvements

--- resources/views/layouts/invoices/index.blade.php
+++ resources/views/layouts/invoices/index.blade.php
@@ -8,8 +8,10 @@
                         <div class="text-info">Total Charge: {{ formatInvoiceTotal($invoice->total) }}</div>
                         @if($invoice->paid)
                             <div class="text-success">Invoice Paid on {{ $invoice->paid_at->toFormattedDateString() }}</div>
-                        @else
+                        @elseif($invoice->due_date->isPast())
                             <div class="text-danger">Payment Due By: {{ $invoice->due_date->toFormattedDateString() }}</div>
+                        @else
+                            <div class="text-warning">Payment Due By: {{ $invoice->due_date->toFormattedDateString() }}</div>
                         @endif
                     </p>
                     @if(Auth::user()->isAdmin())
