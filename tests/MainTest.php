9b5d9efbe72e6c2c2b9b6456d48c0c8889155370
Shivam Paw <shivam@shivampaw.com>
2017-05-29 00:58:28 +0100

    Updated tests

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -2,32 +2,16 @@
 
 use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class MainTest extends BrowserKitTest
 {
     use DatabaseMigrations;
 
-    /**
-     * Add a user and then try to delete it.
-     *
-     * @return void
-     */
-    public function testAddAndDeleteUser()
+    /** @test */
+    public function add_and_delete_user()
     {
-        fwrite(STDERR, print_r(PHP_EOL.'Starting Test', true));
-        fwrite(STDERR, print_r(PHP_EOL.'Number of users: '.User::count(), true));
-        
-        $user = (new StoreUserRequest())->storeUser('phpunit', 'phpunit@example.com');
-        fwrite(STDERR, print_r(PHP_EOL.'Added user with ID: '.$user->id, true));
-       
-        fwrite(STDERR, print_r(PHP_EOL.'Number of users: '.User::count(), true));
-
+        $user = (new StoreUserRequest())->storeUser('phpunit', 'phpunit@example.org');
         $user->delete();
-        fwrite(STDERR, print_r(PHP_EOL.'Deleted user with ID: '.$user->id, true));
-
-        fwrite(STDERR, print_r(PHP_EOL.'Number of users: '.User::count(), true));
     }
 }
