e09d622852c43c9de098b2ac727b8c1ed890a033
Shivam Paw <shivam@shivampaw.com>
2018-04-02 12:19:51 +0100

    Debugging

--- resources/views/layouts/dashboard/admin.blade.php
+++ resources/views/layouts/dashboard/admin.blade.php
@@ -1,2 +1,12 @@
 <p>You are an admin! This means you can manage clients, their invoices and their projects.</p>
 <p>To get started use the navigation menu above!</p>
+
+@if($user->isSuperAdmin())
+    <p>You are a super admin (ID = 1)</p>
+@else
+    <p>You are not a super admin (ID != 1)</p>
+@endif
+
+@if($user->isAdmin())
+    <p>You are a normal admin</p>
+@endif
