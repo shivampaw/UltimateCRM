01f51a938c078867fa038d231939643d6dd30b4c
Shivam Paw <shivam@shivampaw.com>
2019-01-19 19:03:32 +0000

    Implemented project chat functionality
        Projects now have a chat associated with them. Attachments can be added
        to the chat. Attachments must either be images, PDFs, word documents or
        excel sheets.

---
+++ database/migrations/2019_01_19_173227_create_chats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id');
+            $table->integer('user_id');
+            $table->boolean('from_admin');
+            $table->text('message');
+            $table->text('attachment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
