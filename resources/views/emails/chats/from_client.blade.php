c98e2d3885c27b3f8a0147bac69f91c748aeaa6e
Shivam Paw <shivam@shivampaw.com>
2019-01-19 19:49:59 +0000

    Added email notifications for chat messages
    Chat messages now send email notifications to the admins/the client
    depending on who sent the message.

---
+++ resources/views/emails/chats/from_client.blade.php
@@ -0,0 +1,5 @@
+<p>Hi Admins,</p>
+<p>A chat message has just been added to the project entitled <strong>{{ $chat->project->title }}</strong></p>
+<p>The project is for {{ $chat->project->client->name }}.</p>
+<p>You can view this chat by logging on at <strong>{!! url('/') !!}</strong>.</p>
+<p>Thanks!</p>
