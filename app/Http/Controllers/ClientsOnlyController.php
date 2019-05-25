5002caa95afd36a5a359f63460051414166d8d57
Shivam Paw <shivam@shivampaw.com>
2019-05-25 21:34:45 +0100

    bug fix

--- app/Http/Controllers/ClientsOnlyController.php
+++ app/Http/Controllers/ClientsOnlyController.php
@@ -13,98 +13,89 @@ use Stripe\Charge;
 use Stripe\Error\Base;
 use Stripe\Stripe;
 
-class ClientsOnlyController extends Controller
-{
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware('client');
-    }
+class ClientsOnlyController extends Controller{
+	public function __construct() {
+		$this->middleware( 'auth' );
+		$this->middleware( 'client' );
+	}
 
-    public function allInvoices()
-    {
-        $invoices = Auth::user()->client->invoices;
+	public function allInvoices() {
+		$invoices = Auth::user()->client->invoices;
 
-        return view('clientsOnly.invoices.index', compact('invoices'));
-    }
+		return view( 'clientsOnly.invoices.index', compact( 'invoices' ) );
+	}
 
-    public function showInvoice($id)
-    {
-        $invoice = Auth::user()->client->invoices()->findOrFail($id);
+	public function showInvoice( $id ) {
+		$invoice = Auth::user()->client->invoices()->findOrFail( $id );
 
-        return view('clientsOnly.invoices.show', compact('invoice'));
-    }
+		return view( 'clientsOnly.invoices.show', compact( 'invoice' ) );
+	}
 
-    public function payInvoice($id)
-    {
-        $invoice = Auth::user()->client->invoices()->where('paid', false)->findOrFail($id);
+	public function payInvoice( $id ) {
+		$invoice = Auth::user()->client->invoices()->where( 'paid', false )->findOrFail( $id );
 
-        return view('clientsOnly.invoices.pay', compact('invoice'));
-    }
+		return view( 'clientsOnly.invoices.pay', compact( 'invoice' ) );
+	}
 
-    public function paidInvoice(Request $request, $id)
-    {
-        $client = Auth::user()->client;
-        $invoice = $client->invoices()->findOrFail($id);
+	public function paidInvoice( Request $request, $id ) {
+		$client  = Auth::user()->client;
+		$invoice = $client->invoices()->findOrFail( $id );
 
-        Stripe::setApiKey(config('services.stripe.secret'));
+		Stripe::setApiKey( config( 'services.stripe.secret' ) );
 
-        try {
-            $charge = Charge::create([
-                                         'amount'        => $invoice->total,
-                                         'description'   => config('app.name') . ' - Invoice #' . $invoice->id,
-                                         'source'        => $request->stripeToken,
-                                         'currency'      => strtolower(config('crm.currency')),
-                                         'receipt_email' => $client->email,
-                                     ]);
+		try {
+			$charge = Charge::create( [
+				                          'amount'        => $invoice->total,
+				                          'description'   => config( 'app.name' ) . ' - Invoice #' . $invoice->id,
+				                          'source'        => $request->stripeToken,
+				                          'currency'      => ( config( 'crm.currency' ) ),
+				                          'receipt_email' => $client->email,
+			                          ] );
 
-            $invoice->stripe_charge_id = $charge->id;
-            $invoice->paid = true;
-            $invoice->paid_at = Carbon::now();
-            $invoice->save();
+			$invoice->stripe_charge_id = $charge->id;
+			$invoice->paid             = true;
+			$invoice->paid_at          = Carbon::now();
+			$invoice->save();
 
-            Mail::send(new InvoicePaid($client, $invoice));
+			Mail::send( new InvoicePaid( $client, $invoice ) );
 
-            flash('Invoice Paid!');
+			flash( 'Invoice Paid!' );
 
-            return redirect('/invoices/' . $id);
-        } catch (Base $e) {
-            flash($e->getMessage(), 'danger');
+			return redirect( '/invoices/' . $id );
+		} catch ( Base $e ) {
+			flash( $e->getMessage(), 'danger' );
 
-            return back();
-        } catch (Exception $e) {
-            flash('An unknown error occurred. Please try again.', 'danger');
+			return back();
+		} catch ( Exception $e ) {
+			flash( 'An unknown error occurred. Please try again.', 'danger' );
 
-            return back();
-        }
-    }
+			return back();
+		}
+	}
 
-    public function allProjects()
-    {
-        $projects = Auth::user()->client->projects;
-        $projects->load('invoices');
+	public function allProjects() {
+		$projects = Auth::user()->client->projects;
+		$projects->load( 'invoices' );
 
-        return view('clientsOnly.projects.index', compact('projects'));
-    }
+		return view( 'clientsOnly.projects.index', compact( 'projects' ) );
+	}
 
-    public function showProject($id)
-    {
-        $project = Auth::user()->client->projects()->findOrFail($id);
+	public function showProject( $id ) {
+		$project = Auth::user()->client->projects()->findOrFail( $id );
 
-        return view('clientsOnly.projects.show', compact('project'));
-    }
+		return view( 'clientsOnly.projects.show', compact( 'project' ) );
+	}
 
-    public function acceptProject($id)
-    {
-        $project = Auth::user()->client->projects()->findOrFail($id);
-        $project->accepted = true;
-        $project->accepted_at = Carbon::now();
-        $project->save();
+	public function acceptProject( $id ) {
+		$project              = Auth::user()->client->projects()->findOrFail( $id );
+		$project->accepted    = true;
+		$project->accepted_at = Carbon::now();
+		$project->save();
 
-        Mail::send(new ProjectAccepted($project->client, $project));
+		Mail::send( new ProjectAccepted( $project->client, $project ) );
 
-        flash('Project Accepted');
+		flash( 'Project Accepted' );
 
-        return redirect('/projects/' . $project->id);
-    }
+		return redirect( '/projects/' . $project->id );
+	}
 }
