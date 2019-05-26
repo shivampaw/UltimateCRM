2de57adf41a8140878809a4a9aa0302a2627e5e0
Shivam Paw <shivam@shivampaw.com>
2019-05-26 21:17:20 +0100

    Added signed login route to emails for clients

--- resources/views/emails/invoices/new.blade.php
+++ resources/views/emails/invoices/new.blade.php
@@ -1,9 +1,11 @@
 <p>Hi {{ $client->name }},</p>
 <p>An invoice has just been generated for you.</p>
-<p>You can pay for this invoice by logging on at <strong>{!! url('/'); !!}</strong> and viewing your invoices.</p>
+<p>You can pay for this invoice by logging on at
+    <strong>{!! signedLoginUrl($client->user->id, '/invoices/' . $invoice->id) !!}</strong> and
+    viewing your invoices.</p>
 <p>The total charge for this invoice is <strong>{{ formatInvoiceTotal($invoice->total) }}</strong>.</p>
 <p>The invoice is due by <strong>{{ $invoice->due_date->toFormattedDateString() }}</strong>.</p>
 <p>If you have any questions or problems be sure to email me, you can do so by replying to this email.</p>
-<p>Thanks!<br />
-{{ DB::table('users')->find(1)->name }}
+<p>Thanks!<br/>
+    {{ DB::table('users')->find(1)->name }}
 </p>
--- resources/views/emails/projects/accepted.blade.php
+++ resources/views/emails/projects/accepted.blade.php
@@ -1,6 +1,7 @@
 <p>Hi {{ $client->name }},</p>
 <p>You have just accepted the project with the title of {{ $project->title }}.</p>
-<p>You can view the details of this project by logging on at <strong>{!! url('/') !!}</strong>.</p>
+<p>You can view the details of this project by logging on at
+    <strong>{!! signedLoginUrl($client->user->id, 'projects/' . $project->id) !!}</strong>.</p>
 <p>The project PDF has also been attached to this email for you.</p>
 <p>If you have any questions or problems be sure to email me, you can do so by replying to this email.</p>
 <p>Thanks!<br/>
